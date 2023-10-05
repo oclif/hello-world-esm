2c42112b46e1e23fbeff53fec9fe7982e3e16df0
Mike Donnalley <mdonnalley@salesforce.com>
2023-10-05 15:58:17 -0600

    chore: remove test init

--- bin/run.js
+++ bin/run.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 async function main() {
-  const oclif = await import('@oclif/core')
-  await oclif.execute({dir: import.meta.url})
+  const {execute} = await import('@oclif/core')
+  await execute({dir: import.meta.url})
 }
 
 await main()
